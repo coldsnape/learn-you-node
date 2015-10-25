312faa86863e1948179572a06ef8f223421425ab
coldsnape <coldsnape@msn.com>
2015-10-25 05:18:07 +0000

    first commit

--- fileserver.js
+++ fileserver.js
@@ -2,7 +2,7 @@ var http = require('http');
 var fs = require('fs');
 var file = process.argv[2];
 
-var server = http.createServer(function (req, res){
-    fs.createReadStream(file).pipe(res);
-});
-server.listen(8000);
+require('http').createServer(function (req, res) {
+  res.writeHead(200, 'content-type: text/plain')
+  require('fs').createReadStream(process.argv[3]).pipe(res)
+}).listen(process.argv[2] | 0)
